12fb777d8da691c7169362c0960dfd36b3d2e232
Norman Leutner <n.leutner@all2e.com>
2013-09-16 22:13:56 +0200

    added exception handling for wget

--- addons/homematicAudio/translategoogle.tcl
+++ addons/homematicAudio/translategoogle.tcl
@@ -4,7 +4,7 @@
 load tclrega.so
 
 # incluce system variable mapping
-source conf/sysvariables
+source conf/sysvariables.tcl
 
 set command ""
 append command "dom.GetObject(" \" [lindex $argv 0] \" ").Value();"
@@ -14,6 +14,7 @@ set ausgabe [rega $command]
 # urlencode
 source lib/functions.tcl
 init
+
 set encoded [url-encode $ausgabe]
 
 # Aufruf und Erstellung der Datei
@@ -22,10 +23,13 @@ set url http://translate.google.com/translate_tts?tl=de&q=$encoded
 set filename [lindex $argv 0].mp3
 
 # debug
-puts $ausgabe
-puts $encoded
-puts $url
-puts $filename
+#puts $ausgabe
+#puts $encoded
+#puts $url
+#puts $filename
 
-exec ./busybox wget -q -U Mozilla -O sound/$filename "$url"
+if { [catch {exec ./busybox wget -q -U Mozilla -O sound/$filename "$url"} error] } {
+    puts stderr "Could not fetch $filename from $url \n$error"
+    exit 1
+}
 
